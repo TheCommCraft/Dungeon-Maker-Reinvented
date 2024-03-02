4ea27219889a15abab8697489a65cffdb8126f00
TheCommCraft <simon.c.gilde@gmail.com>
2024-03-02 10:34:56 +0100

    0.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/thecommcraft/dungeon-maker-reinvented',
-    packages=find_packages(exclude=[]),
+    packages=["dungeonmaker"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
